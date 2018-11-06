02afa9c9094f511ea70abd057b2b48882d71f6ec
HidehiroNomoto <hoto5252@yahoo.co.jp>
2018-11-06 19:51:38 +0900

    修正

---
+++ CallOfCthulhuAR/Assets/Script/IFButton.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// 2017.x を用いたバイナリが Android 上だとキーボードで日本語入力できない問題のワークアラウンド用.
+/// InputField の子に Button を置き、その Button にアタッチして使用する.
+/// </summary>
+public class IFButton : Button {
+
+    private InputField inputField
+    {
+        get
+        {
+            if (inputField_ == null) {
+                inputField_ = GetComponentInParent<InputField> ();
+            }
+            return inputField_;
+        }
+    }
+    private InputField inputField_;
+
+    private TouchScreenKeyboard keyboard_ = null;
+    private string originalText_ = "";
+    private bool wasCanceled_ = false;
+
+    void CreateKeyboard()
+    {
+        if (keyboard_ != null) return;
+        if (!IsInteractable ())    return;
+
+        if (!inputField.textComponent || inputField.textComponent.font == null)
+            return;
+
+        originalText_ = inputField.text;
+        keyboard_ = TouchScreenKeyboard.Open (inputField.text, TouchScreenKeyboardType.Default, true);
+    }
+
+    void DestroyKeyboard()
+    {
+        if (!inputField.textComponent || !inputField.IsInteractable ())
+            return;            
+
+        if (wasCanceled_) {
+            inputField.text = originalText_;
+            wasCanceled_ = false;
+        }
+
+        if (keyboard_ != null) {
+            keyboard_.active = false;
+            keyboard_ = null;
+            originalText_ = "";
+        }
+    }
+
+    void LateUpdate() {
+        if (keyboard_ == null) return;
+
+        inputField.text = keyboard_.text;
+
+        if (keyboard_.wasCanceled) {
+            wasCanceled_ = true;
+            DestroyKeyboard ();
+        }
+
+        if (keyboard_.done) {
+            DestroyKeyboard ();
+        }
+    }
+
+    public override void OnPointerDown (UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        CreateKeyboard ();
+        base.OnPointerDown (eventData);
+    }
+
+    public override void OnPointerClick (UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        CreateKeyboard ();
+        base.OnPointerClick (eventData);
+    }
+}
